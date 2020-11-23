4add2c8235af561f3319732fc9419d6d8bb40557
Thomas Fiorilla <engineertf@gmail.com>
2020-11-23 13:17:54 -0500

    Basic threading
    Basic threading created for the directory travel.

--- Asst2.c
+++ Asst2.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <pthread.h>
 
 //Function to actually recursively look through 
-void dirRecurse(char * inputDir){
+void * dirRecurse(void * inputDir){
 	DIR * dir = opendir(inputDir);
 	
-	//Check to make sure the opendir() worked
-	if(dir == NULL) return;
+	//Check to make sure the opendir() worked; if it didn't, returns
+	if(dir == NULL){
+		printf("\033[1;31m");
+		printf("Directory %s is inaccessible\n", (char*)inputDir);
+		printf("\033[0m");
+		return NULL;
+	}
+	
+	printf("\033[1;31m");
+	printf("Opening directory %s\n", (char*)inputDir);
+	printf("\033[0m");
 	
 	//Create a string to hold the path, a dirent struct for readdir, and loop while there's more to read within the directory
 	char path[1000];
@@ -21,7 +31,7 @@ void dirRecurse(char * inputDir){
 		//Check to see if the file is a regular file, print the path, file name, and file length in green
 		if(dirRead->d_type == DT_REG){
 			
-			strcpy(path, inputDir);
+			strcpy(path, (char*)inputDir);
 			strcat(path, "/");
 			strcat(path, dirRead->d_name);
 			
@@ -38,7 +48,7 @@ void dirRecurse(char * inputDir){
 		} else if(dirRead->d_type == DT_DIR){
 			//If it is, check to see if it's the current or previous directory and ignore them (".", "..", respectively), then print the path
 			if(strcmp(dirRead->d_name, ".") != 0 && strcmp(dirRead->d_name, "..") != 0){
-				strcpy(path, inputDir);
+				strcpy(path, (char*)inputDir);
 				strcat(path, "/");
 				strcat(path, dirRead->d_name);
 				
@@ -46,8 +56,14 @@ void dirRecurse(char * inputDir){
 				printf("%s\n", path);
 				printf("\033[0m");
 				
-				//The program then recursively calls the path. Ignores .git/ directory because it's annoying
-				if(strcmp(dirRead->d_name, ".git") != 0) dirRecurse(path);
+				//Create a new thread for following subdirectories. ignore .git because it's annoying
+				if(strcmp(dirRead->d_name, ".git") != 0){
+					pthread_t thread;
+					pthread_create(&thread, NULL, dirRecurse, path);
+					
+					//Join the thread once it's done
+					pthread_join(thread, NULL);
+				}
 			}
 		}
 	}
@@ -55,11 +71,19 @@ void dirRecurse(char * inputDir){
 	//Close the DIR pointer
 	closedir(dir);
 	
-	return;
+	return NULL;
 }
 
 int main(int argc, char * argv[]){
-	dirRecurse(".");
+	if(argc == 2){
+		//Create a thread for the initial run; next threads will be opened by the function itself
+		pthread_t thread;
+		pthread_create(&thread, NULL, dirRecurse, argv[1]);
+		
+		//Join the thread once it's done
+		pthread_join(thread, NULL);
+		
+	} else printf("Incorrect number of inputs. Program expects a single directory path as stdin.\n");
 	
 	return 0;
 }
--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all: tokenizer.c Asst2.c
 #	gcc tokenizer.c -o tokenizer
-	gcc Asst2.c -o detector
+	gcc Asst2.c -pthread -o detector
 	
 run: tokenizer.c Asst2.c
 #	gcc tokenizer.c -o tokenizer
-	gcc Asst2.c -o detector
-	./detector
+	gcc Asst2.c -pthread -o detector
+	./detector .
 
 clean:
 	rm -f tokenizer detector *.o
